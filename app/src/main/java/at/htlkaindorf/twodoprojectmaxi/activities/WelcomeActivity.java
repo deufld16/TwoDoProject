8e70b8193d1585787767024178c49a4832897c6e
ma-xi <strmad16@htl-kaindorf.ac.at>
2020-02-24 09:26:58 +0100

    Welcome Screen

---
+++ app/src/main/java/at/htlkaindorf/twodoprojectmaxi/activities/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package at.htlkaindorf.twodoprojectmaxi.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import at.htlkaindorf.twodoprojectmaxi.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
