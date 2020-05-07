91f83a2937b5f37df519954afdd14bbd70be212b
ma-xi <strmad16@htl-kaindorf.ac.at>
2020-05-07 15:54:52 +0200

    Swipe Start

--- app/src/main/java/at/htlkaindorf/twodoprojectmaxi/activities/ToDoListActivity.java
+++ app/src/main/java/at/htlkaindorf/twodoprojectmaxi/activities/ToDoListActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -48,8 +50,29 @@ public class ToDoListActivity extends AppCompatActivity {
     private CategoryListModel clm = new CategoryListModel();
     private BottomNavigationView vNavBottom;
 
-
     private final int RC_CREATION_ACTIVITY = 2;
+
+    ItemTouchHelper.SimpleCallback ithSimpleCallback = new ItemTouchHelper.SimpleCallback(0,
+            ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+
+            if(position == ItemTouchHelper.LEFT)
+            {
+
+            }
+            else if(position == ItemTouchHelper.RIGHT)
+            {
+                
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
