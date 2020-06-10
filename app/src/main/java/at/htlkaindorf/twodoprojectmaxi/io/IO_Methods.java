c6c491ea83ed5d65a6d45b05074b2ea47510cb0c
deufld16 <deufld16@htl-kaindorf.at>
2020-06-10 09:05:51 +0200

    io-classes for transfer added v 1.0

---
+++ app/src/main/java/at/htlkaindorf/twodoprojectmaxi/io/IO_Methods.java
@@ -0,0 +1,31 @@
+package at.htlkaindorf.twodoprojectmaxi.io;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+import at.htlkaindorf.twodoprojectmaxi.beans.Entry;
+import at.htlkaindorf.twodoprojectmaxi.bl.Proxy;
+
+public class IO_Methods {
+
+    public static List<File> convertAudiosToFiles(){
+        List<File> allAudioFiles = new LinkedList<>();
+
+        for (Entry entry:Proxy.getToDoAdapter().getEntries()) {
+            for (String path:
+                 entry.getAllAudioFileLocations()) {
+                try {
+                    File file = new File(path);
+                    allAudioFiles.add(file);
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return allAudioFiles;
+    }
+
+}
